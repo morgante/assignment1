8822bbca2f03b4baa84b3110f6bc7c96919873ab
Morgante Pell <morgante.pell@morgante.net>
2014-02-20 12:36:40 +0400

    start laying it out

---
+++ assignment1.py
@@ -0,0 +1,13 @@
+from datetime import date, timedelta # for date math in EyeTest
+
+import models
+
+def run(customer_list):
+
+# Makes a random customer, for testing
+def make_customer():
+
+
+# This is for testing
+def main():
+	customers = [(date.today(), )]
