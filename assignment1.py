d84f4c7db4bdd6bd4f268537d4896b02ef31269e
Morgante Pell <morgante.pell@morgante.net>
2014-02-27 19:32:02 +0400

    create fake customer factory

--- assignment1.py
+++ assignment1.py
@@ -1,13 +1,38 @@
-from datetime import date, timedelta # for date math in EyeTest
+from models import *
+from faker import Faker
+from datetime import date
 
-import models
+# Make Faker data
+fake = Faker()
 
 def run(customer_list):
+	return 'd;'
 
 # Makes a random customer, for testing
 def make_customer():
+	id = Emirates_ID(fake.first_name(), fake.last_name(), fake.country_code(), "male",
+		fake.date_time_between(start_date="-30y", end_date="-18y"), fake.date_time_this_year(), fake.random_int(min=0, max=9999))
+
+	license = None
+
+	passport = None
+
+	eye_test = None
+
+	translation = None
+
+	customer = Customer(id, license, passport, eye_test, translation)
+
+	return customer;
 
 
 # This is for testing
 def main():
-	customers = [(date.today(), )]
+	customers = []
+
+	for i in range(3):
+		customers.append((date.today(), make_customer()))
+
+	print customers
+
+main()
